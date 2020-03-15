5310a9292d6e59fcc039f33f7d4d2842a21da085
github username <github email address>
2020-03-15 16:33:09 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
 # Add your code here
+
+# Build a class with instance methods.
+# Call instance methods inside of other instance methods.
+# Use instance methods to track information pertinent to an instance of a class.
+
+class Dog
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        self.class.all << self
+    end
+
+    def self.print_all
+        self.all.each do |dog|
+            puts "#{dog.name}"
+        end
+    end
+
+    def self.clear_all
+        self.all.clear
+    end
+
+    
+end
